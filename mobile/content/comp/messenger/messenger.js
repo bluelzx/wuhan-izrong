b6038f6ddd4bc597e1c8252f1e7d70c5bcbf819f
baoyinghai <yhbao@amarsoft.com>
2016-04-25 13:34:01 +0800

    Merge remote-tracking branch 'remotes/origin/develop' into develop
    # Conflicts:
    #	mobile/config.js

--- mobile/content/comp/messenger/messenger.js
+++ mobile/content/comp/messenger/messenger.js
@@ -227,8 +227,6 @@ let Messenger = React.createClass({
         handlePhonePress={this.handlePhonePress}
         handleUrlPress={this.handleUrlPress}
         handleEmailPress={this.handleEmailPress}
-        handleNameCard={this.handleNameCard}
-
         inverted={true}
 
         sessionId={this.props.param.sessionId}
