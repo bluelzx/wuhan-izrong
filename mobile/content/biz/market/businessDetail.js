48e8472f203ee2b29d1717d5fca868a8ba371e16
xgwen <xgwen@amarsoft.com>
2016-04-28 18:17:05 +0800

    Merge remote-tracking branch 'remotes/origin/develop' into develop
    # Conflicts:
    #	mobile/content/biz/market/businessDetail.js

--- mobile/content/biz/market/businessDetail.js
+++ mobile/content/biz/market/businessDetail.js
@@ -48,43 +48,39 @@ let BusinessDetail = React.createClass({
     }
 
     return (
-      <NavBarView navigator={this.props.navigator} fontColor='#ffffff' backgroundColor='#1151B1'
-                  contentBackgroundColor='#18304D' title='业务详情' showBack={true} showBar={true}>
-        <ScrollView style={{backgroundColor:'#194269'}}>
-          <View style={{backgroundColor:'#194269'}}>
-            <View style={{marginLeft:10}}>
-              {this.returnItem('业务类型:', (this.state.detailData.bizCategoryDesc + '-' + this.state.detailData.bizItemDesc))}
-              {this.returnItem('方向:', this.state.detailData.bizOrientationDesc)}
-              {this.returnItem('期限:', this.state.detailData.term == null || this.state.detailData.term == 0 ? '--' : this.state.detailData.term + '天')}
-              {this.returnItem('金额:', this.state.detailData.amount == null || this.state.detailData.amount == 0 ? '--'
-                : this.state.detailData.amount <= 100000000 ? this.state.detailData.amount / 10000 + '万' : this.state.detailData.amount / 100000000 + '亿')}
-              {this.returnItem('利率:', this.state.detailData.rate == null || this.state.detailData.rate == 0? '--' :this.state.detailData.rate * 100 + '%')}
-              {this.returnItem('备注:', this.state.detailData.remark == null || this.state.detailData.remark == 0? '--' : this.state.detailData.remark)}
-              {this.returnItem('更新时间:', this.state.lastModifyDate)}
-            </View>
-            {this.renderAdjunct()}
-            <View style={{backgroundColor:'#153757',borderRadius:2,margin:10}}>
-              {this.renderPromulgator()}
-              {this.returnInfoItem(require('../../image/market/tel.png'), this.state.bizOrderOwnerBean.phoneNumber, this.state.bizOrderOwnerBean.isPublicPhone)}
-              {this.returnInfoItem(require('../../image/market/mobile.png'), this.state.bizOrderOwnerBean.mobileNumber, this.state.bizOrderOwnerBean.isPublicMobile)}
-              {this.returnInfoItem(require('../../image/market/QQ.png'), this.state.bizOrderOwnerBean.qqNo, this.state.bizOrderOwnerBean.isPublicQQNo)}
-              {this.returnInfoItem(require('../../image/market/weChat.png'), this.state.bizOrderOwnerBean.weChatNo, this.state.bizOrderOwnerBean.isPublicWeChatNo)}
-              {this.returnInfoItem(require('../../image/market/org.png'), this.state.bizOrderOwnerBean.orgName)}
-            </View>
+      <ScrollView style={{backgroundColor:'#194269'}}>
+        <View style={{backgroundColor:'#194269'}}>
+          <View style={{marginLeft:10}}>
+            {this.returnItem('业务类型:', (this.state.detailData.bizCategoryDesc + '-' + this.state.detailData.bizItemDesc))}
+            {this.returnItem('方向:', this.state.detailData.bizOrientationDesc)}
+            {this.returnItem('期限:', this.state.detailData.term == null || this.state.detailData.term == 0 ? '--' : this.state.detailData.term + '天')}
+            {this.returnItem('金额:', this.state.detailData.amount == null || this.state.detailData.amount == 0 ? '--'
+              : this.state.detailData.amount <= 100000000 ? this.state.detailData.amount / 10000 + '万' : this.state.detailData.amount / 100000000 + '亿')}
+            {this.returnItem('利率:', this.state.detailData.rate == null || this.state.detailData.rate == 0 ? '--' : this.state.detailData.rate * 100 + '%')}
+            {this.returnItem('备注:', this.state.detailData.remark == null || this.state.detailData.remark == 0 ? '--' : this.state.detailData.remark)}
+            {this.returnItem('更新时间:', this.state.lastModifyDate)}
           </View>
-        </ScrollView>
-      </NavBarView>
-    )
+          {this.renderAdjunct()}
+          <View style={{backgroundColor:'#153757',borderRadius:2,margin:10}}>
+            {this.renderPromulgator()}
+            {this.returnInfoItem(require('../../image/market/tel.png'), this.state.bizOrderOwnerBean.phoneNumber, this.state.bizOrderOwnerBean.isPublicPhone)}
+            {this.returnInfoItem(require('../../image/market/mobile.png'), this.state.bizOrderOwnerBean.mobileNumber, this.state.bizOrderOwnerBean.isPublicMobile)}
+            {this.returnInfoItem(require('../../image/market/QQ.png'), this.state.bizOrderOwnerBean.qqNo, this.state.bizOrderOwnerBean.isPublicQQNo)}
+            {this.returnInfoItem(require('../../image/market/weChat.png'), this.state.bizOrderOwnerBean.weChatNo, this.state.bizOrderOwnerBean.isPublicWeChatNo)}
+            {this.returnInfoItem(require('../../image/market/org.png'), this.state.bizOrderOwnerBean.orgName)}
+          </View>
+        </View>
+      </ScrollView>
+    );
   },
 
   render: function () {
-
     return (
       <NavBarView navigator={this.props.navigator} fontColor='#ffffff' backgroundColor='#1151B1'
                   contentBackgroundColor='#18304D' title='业务详情' showBack={true} showBar={true}>
         {this._renderContent()}
       </NavBarView>
-    )
+    );
   },
 
   returnItem: function (desc, value) {
@@ -93,7 +89,7 @@ let BusinessDetail = React.createClass({
         <Text style={{fontSize:16,color:'white',flex:1}}>{desc}</Text>
         <Text style={{fontSize:16,color:(desc == '更新时间:')?'#ffd547':'white',width:235/375*screenWidth}}>{value}</Text>
       </View>
-    )
+    );
   },
   renderAdjunct: function () {
     return (
@@ -101,7 +97,7 @@ let BusinessDetail = React.createClass({
         {this.renderImageTitle()}
         {this.renderImageItem()}
       </View>
-    )
+    );
   },
   renderPromulgator: function () {
     return (
@@ -115,7 +111,7 @@ let BusinessDetail = React.createClass({
           <Text style={{fontSize:12,color:'#68bbaa'}}>{'(点击洽谈)'}</Text>
         </TouchableHighlight>
       </View>
-    )
+    );
   },
   returnInfoItem: function (url, value, isPublic) {
     if (isPublic) {
@@ -126,11 +122,11 @@ let BusinessDetail = React.createClass({
           />
           <Text style={{marginLeft:10,fontSize:16,color:'white',width:screenWidth - 60}}>{value}</Text>
         </View>
-      )
+      );
     } else {
       return (
         <View></View>
-      )
+      );
     }
   },
   renderImageTitle(){
