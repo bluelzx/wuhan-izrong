213c900b998edd16858300e5d49578d83e53238d
baoyinghai <yhbao@amarsoft.com>
2016-04-28 21:46:23 +0800

    Merge remote-tracking branch 'remotes/origin/develop' into develop
    # Conflicts:
    #	mobile/config.js

--- mobile/config.js
+++ mobile/config.js
@@ -2,8 +2,8 @@ let Config = {
   Dev: true, // Switch for log. true means print.
   Host: 'http://192.168.64.205:9101/fas',
   //Host: 'http://192.168.64.133:9081/fas',
-  //ImHost:'192.168.61.84:4000',
-  ImHost:'192.168.64.94:3000',
+  ImHost:'192.168.64.148:3000',
+  //ImHost:'192.168.64.94:3000',
   //ImHost:'192.168.64.197'
   //ImHost: '192.168.64.148:3000'
 };
