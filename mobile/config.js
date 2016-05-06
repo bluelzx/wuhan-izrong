667c558f627b383377c29e5da1a53088656d69e4
swei1 <swei1@amarsoft.com>
2016-05-06 09:55:40 +0800

    feat(system) update  config

--- mobile/config.js
+++ mobile/config.js
@@ -1,6 +1,6 @@
 
 var developConfig = {
-  Dev: true, // Switch for log. true means print.
+  Dev: false, // Switch for log. true means print.
   Host: 'http://192.168.64.205:9101/fas',
   //Host: 'http://192.168.64.248:9081/fas',
   //Host: 'http://192.168.64.216:9081/fas',
@@ -27,14 +27,18 @@ var productConfig = {
   //ImHost: '192.168.64.223:3000'
 };
 
-module.exports = function () {
-  switch(process.env.NODE_ENV){
-    case 'develop':
-      return developConfig;
-    case 'product':
-      return productConfig;
-    default :
-      return productConfig;
-  }
-};
+var Config = developConfig;
+
+module.exports = Config;
+//module.exports = function () {
+//  //switch(process.env.NODE_ENV){
+//  //  case 'develop':
+//  //    return developConfig;
+//  //  case 'product':
+//  //    return productConfig;
+//  //  default :
+//  //    return productConfig;
+//  //}
+//  return {...developConfig};
+//};
 
