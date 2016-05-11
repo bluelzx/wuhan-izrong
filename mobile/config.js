239d2235ddface8c94909e5e940c1431f389d158
xgwen <xgwen@amarsoft.com>
2016-05-11 16:31:51 +0800

    feat(): modify logic code about publish,myBusiness

--- mobile/config.js
+++ mobile/config.js
@@ -1,7 +1,7 @@
 
 var developConfig = {
   Dev: true, // Switch for log. true means print.
-  Host: 'http://192.168.64.205:9101/fas',
+  Host: 'http://192.168.64.205:9102/fas',
   ImHost:'192.168.61.84:4000'//dev
 };
 
