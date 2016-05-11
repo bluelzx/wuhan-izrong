d46b8def39fce75f6f714b0ed14fe91f050f7b7a
xgwen <xgwen@amarsoft.com>
2016-05-11 17:15:20 +0800

    feat(): updata bizDetail

--- mobile/content/biz/market/businessDetail.js
+++ mobile/content/biz/market/businessDetail.js
@@ -34,25 +34,27 @@ let { MARKET_CHANGE } = require('../../constants/dictEvent');
 let BusinessDetail = React.createClass({
   getInitialState(){
     let marketInfo = this.props.param.marketInfo;
-    let userId = AppStore.getUserId();
+    let loginUserInfo = AppStore.getLoginUserInfo();
     let userInfo = MarketStore.getUserInfoByUserId(marketInfo.userId);
 
+    if (loginUserInfo.userId == marketInfo.userId){
+      userInfo = loginUserInfo;
+    }
+
+
     return {
-      userId: userId,
+      userId: loginUserInfo.userId,
       detailData: '',
       bizOrderOwnerBean: '',
       fileUrlList: [],
       marketInfo: marketInfo,
       orderUserId: marketInfo.userId,
-      lastModifyDate: '',
+      lastModifyDate: marketInfo.lastModifyDate,
       userInfo: userInfo
     }
   },
 
   componentDidMount() {
-    InteractionManager.runAfterInteractions(() => {
-      this.getBizOrderInMarket(this.state.marketInfo.id);
-    });
   },
 
   componentWillUnmount: function () {
@@ -87,17 +89,17 @@ let BusinessDetail = React.createClass({
           <View style={{backgroundColor:'#f7f7f7',height:10}}>
           </View>
           <View style={{marginLeft:10}}>
-            {this.returnItem('业务类型:', (this.state.detailData.bizCategoryDesc))}
-            {this.returnItem('方向:', this.state.detailData.bizOrientationDesc)}
-            {this.returnItem('期限:', this.termChangeHelp(this.state.detailData.term))}
-            {this.returnItem('金额:', this.state.detailData.amount == null || this.state.detailData.amount == 0 ? '--'
-              : this.state.detailData.amount < 100000000 ? (this.state.detailData.amount / 10000) + '万' : (this.state.detailData.amount / 100000000) + '亿')}
-            {this.returnItem('利率:', this.state.detailData.rate == null || this.state.detailData.rate == 0 ? '--' : numeral(this.state.detailData.rate * 100).format('0,0.00') + '%')}
-            {this.returnItem('备注:', this.state.detailData.remark == null || this.state.detailData.remark == 0 ? '--' : this.state.detailData.remark)}
-            {this.returnItem('更新时间:', this.state.lastModifyDate)}
+            {this.returnItem('业务类型:', (this.state.marketInfo.bizCategoryDesc))}
+            {this.returnItem('方向:', this.state.marketInfo.bizOrientationDesc)}
+            {this.returnItem('期限:', this.termChangeHelp(this.state.marketInfo.term))}
+            {this.returnItem('金额:', this.state.marketInfo.amount == null || this.state.marketInfo.amount == 0 ? '--'
+              : this.state.marketInfo.amount < 100000000 ? (this.state.marketInfo.amount / 10000) + '万' : (this.state.marketInfo.amount / 100000000) + '亿')}
+            {this.returnItem('利率:', this.state.marketInfo.rate == null || this.state.marketInfo.rate == 0 ? '--' : numeral(this.state.marketInfo.rate * 100).format('0,0.00') + '%')}
+            {this.returnItem('备注:', this.state.marketInfo.remark == null || this.state.marketInfo.remark == 0 ? '--' : this.state.marketInfo.remark)}
+            {this.returnItem('更新时间:', this.state.marketInfo.lastModifyDate)}
           </View>
           {this.renderAdjunct()}
-          {this.renderUserInfo(this.state.userInfo)}
+          {this.renderUserInfo()}
         </View>
       </ScrollView>
     );
@@ -136,11 +138,11 @@ let BusinessDetail = React.createClass({
     return (
       <View style={{backgroundColor:'#f0f0f0',borderRadius:2,margin:10}}>
         {this.renderPromulgator(userInfo)}
-        {this.returnInfoItem(require('../../image/market/email.png'), userInfo.email, true)}
-        {this.returnInfoItem(require('../../image/market/tel.png'), userInfo.phoneNumber == null ? '未填写' : userInfo.phoneNumber, userInfo.publicPhone)}
-        {this.returnInfoItem(require('../../image/market/mobile.png'), userInfo.mobileNumber == null ? '未填写' : userInfo.mobileNumber, userInfo.publicMobile)}
-        {this.returnInfoItem(require('../../image/market/QQ.png'), userInfo.qqNo == null ? '未填写' : userInfo.qqNo, userInfo.publicQQ)}
-        {this.returnInfoItem(require('../../image/market/weChat.png'), userInfo.weChatNo == null ? '未填写' : userInfo.weChatNo, userInfo.publicWeChat)}
+        {this.returnInfoItem(require('../../image/market/email.png'), this.state.userInfo.email, true)}
+        {this.returnInfoItem(require('../../image/market/tel.png'), this.state.userInfo.phoneNumber == null ? '未填写' : this.state.userInfo.phoneNumber, this.state.userInfo.publicPhone)}
+        {this.returnInfoItem(require('../../image/market/mobile.png'), this.state.userInfo.mobileNumber == null ? '未填写' : this.state.userInfo.mobileNumber, this.state.userInfo.publicMobile)}
+        {this.returnInfoItem(require('../../image/market/QQ.png'), this.state.userInfo.qqNo == null ? '未填写' : this.state.userInfo.qqNo, this.state.userInfo.publicQQ)}
+        {this.returnInfoItem(require('../../image/market/weChat.png'), this.state.userInfo.weChatNo == null ? '未填写' : this.state.userInfo.weChatNo, this.state.userInfo.publicWeChat)}
       </View>
     );
   },
@@ -165,7 +167,7 @@ let BusinessDetail = React.createClass({
       );
     }
   },
-  renderUserPhoto: function (userInfo) {
+  renderUserPhoto: function () {
     if (userInfo.photoFileUrl == null) {
       return (
         <View style={{margin:10}}>
@@ -177,7 +179,7 @@ let BusinessDetail = React.createClass({
         <View style={{margin:10}}>
           <Image
             style={{height:36,width:36,borderRadius:18}}
-            source={{uri:userInfo.photoFileUrl}}
+            source={{uri:this.state.userInfo.photoFileUrl}}
           />
         </View>
       );
