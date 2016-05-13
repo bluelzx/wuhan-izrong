ac4e6674030692fac3915e002be3d109dc8a6b12
baoyinghai <yhbao@amarsoft.com>
2016-05-13 09:36:04 +0800

    backward: linghtBox to View

--- mobile/config.js
+++ mobile/config.js
@@ -1,12 +1,14 @@
-
 var developConfig = {
   Dev: true, // Switch for log. true means print.
-  //Host: 'http://192.168.64.205:9101/fas',
   Host: 'http://192.168.64.205:9102/fas',   //dev
-  //Host: 'http://192.168.64.205:9101/fas',     //qa
-  //Host: 'http://192.168.64.248:9081/fas',
-  ImHost:'192.168.61.84:4000'//dev
+  ImHost:'192.168.61.84:5000'//dev
 };
+
+var qaConfig = {
+  Dev: false,
+  Host: 'http://192.168.64.205:9101/fas',     //qa
+  ImHost:'192.168.61.84:4000'//qa
+}
 
 var productConfig = {
   Dev: false, // Switch for log. true means print.
@@ -14,7 +16,7 @@ var productConfig = {
   ImHost: '139.196.174.42:4000'
 };
 
-var Config = developConfig;
+var Config = qaConfig;
 
 module.exports = Config;
 //module.exports = function () {
