3ee9265148a31665f3962daca061956c126cecc6
baoyinghai <yhbao@amarsoft.com>
2016-06-08 15:53:59 +0800

    fix: file move change to copy

--- mobile/android/app/src/main/java/com/fasapp/utils/FileUtils.java
+++ mobile/android/app/src/main/java/com/fasapp/utils/FileUtils.java
@@ -367,7 +367,8 @@ public class FileUtils {
         }
         File oldFile = new File(oldPath);
         File newFile = new File(newPath);
-        oldFile.renameTo(newFile);
+        copyFile(oldPath, newPath, true);
+      //  oldFile.renameTo(newFile);
         return newFile.getAbsolutePath();
     }
 
@@ -393,5 +394,81 @@ public class FileUtils {
             e.printStackTrace();
         }
     }
+
+
+
+
+        /**
+         * 复制单个文件
+         *
+         * @param srcFileName
+         *            待复制的文件名
+         * @param destFileName
+         *            目标文件名
+         * @param overlay
+         *            如果目标文件存在，是否覆盖
+         * @return 如果复制成功返回true，否则返回false
+         */
+        public static boolean copyFile(String srcFileName, String destFileName,
+                boolean overlay) {
+            File srcFile = new File(srcFileName);
+
+            // 判断源文件是否存在
+            if (!srcFile.exists()) {
+                System.out.println("不存在");
+                return false;
+            } else if (!srcFile.isFile()) {
+                System.out.println("不是一个文件");
+                return false;
+            }
+
+            // 判断目标文件是否存在
+            File destFile = new File(destFileName);
+            if (destFile.exists()) {
+                // 如果目标文件存在并允许覆盖
+                if (overlay) {
+                    // 删除已经存在的目标文件，无论目标文件是目录还是单个文件
+                    new File(destFileName).delete();
+                }
+            } else {
+                // 如果目标文件所在目录不存在，则创建目录
+                if (!destFile.getParentFile().exists()) {
+                    // 目标文件所在目录不存在
+                    if (!destFile.getParentFile().mkdirs()) {
+                        // 复制文件失败：创建目标文件所在目录失败
+                        return false;
+                    }
+                }
+            }
+
+            // 复制文件
+            int byteread = 0; // 读取的字节数
+            InputStream in = null;
+            OutputStream out = null;
+
+            try {
+                in = new FileInputStream(srcFile);
+                out = new FileOutputStream(destFile);
+                byte[] buffer = new byte[1024];
+
+                while ((byteread = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, byteread);
+                }
+                return true;
+            } catch (FileNotFoundException e) {
+                return false;
+            } catch (IOException e) {
+                return false;
+            } finally {
+                try {
+                    if (out != null)
+                        out.close();
+                    if (in != null)
+                        in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
 }
